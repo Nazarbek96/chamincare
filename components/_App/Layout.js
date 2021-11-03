bff90bfa49bfcacc9fc538877b5c67ad4594d1e5
Nazarbek <nazarbeksarkyt@gmail.com>
2021-11-03 14:34:46 +0900

    add meta changes

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -9,9 +9,9 @@ const Layout = ({ children }) => {
                 <title>Chamincare</title>
                 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=eb2169a44e3f07a93e88112db1affb0f"></script>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <meta name="description" content="차민내과 - 환자 중심의 의료 서비스" />
                 <meta name="og:title" property="og:title" content="차민내과 - 환자 중심의 의료 서비스"></meta>
-                <meta name="twitter:card" content="차민내과 - 환자 중심의 의료 서비스"></meta>
+                <meta name="og:description" property="og:description" content="수원시 팔달구 일월로 22번길 7 " />
+                {/* <meta name="twitter:card" content="차민내과 - 환자 중심의 의료 서비스"></meta> */}
                 {/* <link rel="canonical" href="https://disin-react.envytheme.com/"></link> */}
             </Head>
 
