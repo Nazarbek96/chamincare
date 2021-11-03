54d87c67a7cf6bacbab23455c72cc33801d3c987
Nazarbek <nazarbeksarkyt@gmail.com>
2021-11-03 13:49:31 +0900

    add meta description

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -9,10 +9,10 @@ const Layout = ({ children }) => {
                 <title>Chamincare</title>
                 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=eb2169a44e3f07a93e88112db1affb0f"></script>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <meta name="description" content="Disin - React Next Medical & Doctors Template" />
-                <meta name="og:title" property="og:title" content="Disin - React Next Medical & Doctors Template"></meta>
-                <meta name="twitter:card" content="Disin - React Next Medical & Doctors Template"></meta>
-                <link rel="canonical" href="https://disin-react.envytheme.com/"></link>
+                <meta name="description" content="차민내과 - 환자 중심의 의료 서비스" />
+                <meta name="og:title" property="og:title" content="차민내과 - 환자 중심의 의료 서비스"></meta>
+                <meta name="twitter:card" content="차민내과 - 환자 중심의 의료 서비스"></meta>
+                {/* <link rel="canonical" href="https://disin-react.envytheme.com/"></link> */}
             </Head>
 
             {children}
